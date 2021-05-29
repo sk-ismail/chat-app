75a26c042586f30dc9c7412ba878e9642922b808
sk-ismail <kingofdemons077@gmail.com>
2021-05-29 18:44:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express= require('express')
+const app= express();
+const cors= require('cors');
+const server=require('http').createServer(app)
+const io=require('socket.io')(server)
+port=5000;
+
+app.use(express.static('views'))
+app.use(cors())
+
+
+server.listen(port, ()=>{
+    console.log("listening...")
+})
+
+
+io.on('connection', (socket)=>{
+    //console.log(socket.id)
+
+    socket.on('message', (data)=>{
+
+        socket.broadcast.emit('message', data)
+    });
+});
